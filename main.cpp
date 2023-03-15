6d88904fbfa540c4101a15218ab7ef233d5c8231
diamxnd <121052260+diamxncl@users.noreply.github.com>
2023-03-15 20:51:21 +0100

    V2.0 Final

--- main.cpp
+++ main.cpp
@@ -4,20 +4,23 @@
 using namespace std;
 
 int main () {
+    system("clear");
+    cout << "\033[32m" << "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - " << "\033[0m" << endl;
+    cout << endl << "\033[32m" << "[+] Comenzando actualizaciones e instalación de docker." << "\033[0m" << endl;
     system("sudo apt update");
     usleep(50000);
     system("sudo apt install docker.io docker-compose -y");
 
     system("clear");
     cout << endl << "\033[32m" << "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - " << "\033[0m" << endl;
-    cout << endl << "\033[32m" << "[+] Actualizaciones e instalación de docker realizada." << "\033[0m" << endl;
+    cout << endl << "\033[32m" << "[*] Actualizaciones e instalación de docker realizada." << "\033[0m" << endl;
     cout << endl << "\033[32m" << "[+] Preparando la instalación del docker..." << "\033[0m" << endl << endl;
-    sleep(1);
+    usleep(500000);
 
-    system("clear");
     system("docker-compose up -d");
     cout << endl << "\033[32m" << "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - " << "\033[0m" << endl;
     cout << "\033[32m" << "[+] Docker creado." << endl << endl << "[+] Mostrando configuración peer..." << "\033[0m" << endl << endl;
+
     system("curl ifconfig.me > ip_publica.txt");
     sleep(2);
     system("sed -i \"s/wireguard.domain.com/$(cat ip_publica.txt)/g\" ./config/peer1/peer1.conf");
