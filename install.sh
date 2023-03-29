d4f7e0b33ad40b814b77d573b2ed66f7ba90befb
diamxnd <121052260+diamxncl@users.noreply.github.com>
2023-03-29 10:25:55 +0200

    V2.0 - Stable

---
+++ install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+echo "cargando..."
+sudo apt-get update -y > /dev/null && sudo apt-get upgrade -y > /dev/null 
+sudo apt-get install figlet curl -y > /dev/null
+
+figlet wireguard
+echo ""
+echo "$(tput setaf 7)[$(tput setaf 1)+$(tput setaf 7)] Instalando docker."
+
+sudo apt-get install docker.io docker-compose -y > /dev/null
+
+clear
+figlet wireguard
+echo ""
+echo "$(tput setaf 7)[$(tput setaf 1)*$(tput setaf 7)] Docker instalado."
+echo ""
+echo "$(tput setaf 7)[$(tput setaf 1)+$(tput setaf 7)] Creando docker wireguard."
+echo ""
+sleep 0.5
+
+docker-compose up -d > /dev/null
+
+clear
+figlet wireguard
+echo ""
+echo "$(tput setaf 7)[$(tput setaf 1)*$(tput setaf 7)] Docker instalado."
+echo ""
+echo "$(tput setaf 7)[$(tput setaf 1)*$(tput setaf 7)] Docker creado."
+echo ""
+echo "$(tput setaf 7)[$(tput setaf 1)+$(tput setaf 7)] Mostrando configuación peer:"
+echo ""
+curl ifconfig.me > ip_publica.txt
+sleep 0.5
+
+clear
+figlet wireguard
+echo ""
+echo "$(tput setaf 7)[$(tput setaf 1)*$(tput setaf 7)] Docker instalado."
+echo ""
+echo "$(tput setaf 7)[$(tput setaf 1)*$(tput setaf 7)] Docker creado."
+echo ""
+echo "$(tput setaf 7)[$(tput setaf 1)+$(tput setaf 7)] Mostrando configuación peer:"
+echo ""
+
+sleep 1.5
+
+sed -i 's/wireguard.domain.com/$(cat ip_publica.txt)/g' ./config/peer1/peer1.conf
+
+sleep 0.5
+
+cat ./config/peer1/peer1.conf
+echo ""
+
